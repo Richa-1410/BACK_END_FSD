bf564c39ed9f699b5f8f0a170315ce6827afcd2a
Richa-1410 <richaverma6161@gmail.com>
2026-07-21 11:30:39 +0530

    arrowfn

---
+++ es6.js
@@ -0,0 +1,16 @@
+//function es 6 basic
+//parameter function
+
+
+function hello(a,b) {
+    console.log("Hello, World!");
+    return a+b;
+}
+console.log(hello(5, 10));
+//arrow function
+//() => {}
+const hellos = (a, b) => {
+    console.log("Hello, World!");
+    return a + b;
+}
+console.log(hellos(25, 10));
